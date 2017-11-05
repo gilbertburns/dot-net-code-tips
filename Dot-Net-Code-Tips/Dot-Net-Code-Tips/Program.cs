b269a7336f110e35d918dc4bafb7f225c231a10c
Gil Burns <gkburns@yahoo.com>
2017-11-05 09:54:17 -0600

    Initial check-in.

---
+++ Dot-Net-Code-Tips/Dot-Net-Code-Tips/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_Net_Code_Tips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
